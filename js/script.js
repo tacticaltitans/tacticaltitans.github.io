6c940ae117072cdb8b03057912f32ff4cd480713
Stageddat <minecraftforevergood@gmail.com>
2023-04-23 13:50:11 +0200

    Menu update!!

--- js/script.js
+++ js/script.js
@@ -29,9 +29,7 @@
 			owl: $(".owl-carousel"),
 			swiper: $(".swiper-slider"),
 			counter: $(".counter"),
-			progressLinear: $(".progress-linear"),
 			circleProgress: $(".progress-bar-circle"),
-			preloader: $(".preloader"),
 			captcha: $('.recaptcha'),
 			lightGallery: $("[data-lightgallery='group']"),
 			lightGalleryItem: $("[data-lightgallery='item']"),
@@ -88,29 +86,6 @@
 	$(function () {
 		isNoviBuilder = window.xMode;
 
-		// Page loader & Page transition
-		if (plugins.preloader.length && !isNoviBuilder) {
-			pageTransition({
-				target: document.querySelector('.page'),
-				delay: 100,
-				duration: 500,
-				classIn: 'fadeIn',
-				classOut: 'fadeOut',
-				classActive: 'animated',
-				conditions: function (event, link) {
-					return !/(\#|callto:|tel:|mailto:|:\/\/)/.test(link) && !event.currentTarget.hasAttribute('data-lightgallery');
-				},
-				onTransitionStart: function (options) {
-					setTimeout(function () {
-						plugins.preloader.removeClass('loaded');
-					}, options.duration * .75);
-				},
-				onReady: function () {
-					plugins.preloader.addClass('loaded');
-					windowReady = true;
-				}
-			});
-		}
 
 		/**
 		 * @desc Calculate the height of swiper slider basing on data attr
@@ -384,13 +359,6 @@
 			}
 		}
 
-		// Page loader
-		if (plugins.preloader.length) {
-			loaderTimeoutId = setTimeout(function () {
-				if (!windowReady && !isNoviBuilder) plugins.preloader.removeClass('loaded');
-			}, 2000);
-		}
-
 		// UI To Top
 		if (isDesktop && !isNoviBuilder) {
 			$().UItoTop({
@@ -406,61 +374,6 @@
 				plugins.owl[i].owl = c;
 
 				initOwlCarousel(c);
-			}
-		}
-
-		// RD Navbar
-		if (plugins.rdNavbar.length) {
-			var aliaces, i, j, len, value, values, responsiveNavbar;
-
-			aliaces = ["-", "-sm-", "-md-", "-lg-", "-xl-", "-xxl-"];
-			values = [0, 576, 768, 992, 1200, 1600];
-			responsiveNavbar = {};
-
-			for (i = j = 0, len = values.length; j < len; i = ++j) {
-				value = values[i];
-				if (!responsiveNavbar[values[i]]) {
-					responsiveNavbar[values[i]] = {};
-				}
-				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'layout')) {
-					responsiveNavbar[values[i]].layout = plugins.rdNavbar.attr('data' + aliaces[i] + 'layout');
-				}
-				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'device-layout')) {
-					responsiveNavbar[values[i]]['deviceLayout'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'device-layout');
-				}
-				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'hover-on')) {
-					responsiveNavbar[values[i]]['focusOnHover'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'hover-on') === 'true';
-				}
-				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'auto-height')) {
-					responsiveNavbar[values[i]]['autoHeight'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'auto-height') === 'true';
-				}
-
-				if (isNoviBuilder) {
-					responsiveNavbar[values[i]]['stickUp'] = false;
-				} else if (plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up')) {
-					responsiveNavbar[values[i]]['stickUp'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up') === 'true';
-				}
-
-				if (plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up-offset')) {
-					responsiveNavbar[values[i]]['stickUpOffset'] = plugins.rdNavbar.attr('data' + aliaces[i] + 'stick-up-offset');
-				}
-			}
-
-
-			plugins.rdNavbar.RDNavbar({
-				anchorNav: !isNoviBuilder,
-				stickUpClone: (plugins.rdNavbar.attr("data-stick-up-clone") && !isNoviBuilder) ? plugins.rdNavbar.attr("data-stick-up-clone") === 'true' : false,
-				responsive: responsiveNavbar,
-				callbacks: {
-					onDropdownOver: function () {
-						return !isNoviBuilder;
-					},
-				}
-			});
-
-
-			if (plugins.rdNavbar.attr("data-body-class")) {
-				document.body.className += ' ' + plugins.rdNavbar.attr("data-body-class");
 			}
 		}
 
@@ -578,11 +491,6 @@
 			new WOW().init();
 		}
 
-		// RD Input Label
-		if (plugins.rdInputLabel.length) {
-			plugins.rdInputLabel.RDInputLabel();
-		}
-
 		// Material Parallax
 		if (plugins.materialParallax.length) {
 			if (!isNoviBuilder && !isIE && !isMobile) {
@@ -608,3 +516,24 @@
 		}
 	});
 }());
+
+// NAVEGACION
+// Función para cerrar la barra de navegación
+function closeNavigationMenu() {
+	$(".navigation-menu").removeClass("active");
+  }
+  
+  // Escuchar el evento "click" en el documento
+  $(document).on("click", function(event) {
+	// Comprobar si el elemento que se ha hecho clic es la barra de navegación o uno de sus hijos
+	if ($(event.target).closest(".navigation-menu").length === 0 && $(event.target).closest(".menu-toggle-btn").length === 0) {
+	  // Si el elemento no es la barra de navegación o uno de sus hijos, cerrar la barra de navegación
+	  closeNavigationMenu();
+	}
+  });
+  
+  // Escuchar el evento "click" en el botón de alternancia de menú
+  $(".menu-toggle-btn").click(function(){
+	$(this).toggleClass("fa-times");
+	$(".navigation-menu").toggleClass("active");
+  });
